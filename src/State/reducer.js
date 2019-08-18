26f4a13cdac4c7830a1a74d9a8354376173c7caf
M Kumaran <veerammalkumaran@gmail.com>
2019-08-18 17:56:54 +0530

    testing 2

--- src/State/reducer.js
+++ src/State/reducer.js
@@ -1,11 +1,26 @@
-import { NEW_ADDRESS_CHANGE } from "./../State/ActionTypes";
+import { ADDRESS_CHANGE, ADD_NEW_BUTTON_CLICKED } from "./../State/ActionTypes";
 
 export default function reducer(state, action) {
   console.log(action);
 
   switch (action.type) {
-    case NEW_ADDRESS_CHANGE:
-      return { ...state, newInput: { ...state.newInput, address: action.data } };
+    case ADD_NEW_BUTTON_CLICKED:
+      return { ...state, isAddMode: true };
+
+    case ADDRESS_CHANGE: {
+      const id = action.data.id;
+
+      const updatedMarkers = state.markers.map((marker, i) => {
+        if (marker.id === id) {
+          return { ...marker, address: action.data.text };
+        }
+      });
+
+      return {
+        ...state,
+        markers: updatedMarkers
+      };
+    }
     case "SHOW_COMPLETE":
       return "COMPLETE";
     case "SHOW_INCOMPLETE":
--- src/__tests__/state.js
+++ src/__tests__/state.js
@@ -2,18 +2,36 @@ import React from "react";
 
 //State
 import reducer from "./../State/reducer";
-import initialState from "./../State/initialState";
-import { ADD_NEW_BUTTON_CLICKED } from "./../State/ActionTypes";
+import { initialState, markerState } from "./../State/initialState";
+import { ADD_NEW_BUTTON_CLICKED, ADDRESS_CHANGE } from "./../State/ActionTypes";
 
 // testing library
 import { render, fireEvent, waitForElement } from "@testing-library/react";
 
+// initial state for testing
+const marker = { ...markerState };
+const state = {
+  ...initialState,
+  markersId: [1],
+  markers: [{ ...marker, id: 1 }]
+};
+
 describe("Testing all state related methods", () => {
   it("Clicking add new button update the state", () => {
-    const state = { ...initialState };
     const action = { type: ADD_NEW_BUTTON_CLICKED };
     const updatedState = reducer(state, action);
 
     expect(updatedState.isAddMode).toBe(true);
+  });
+
+  it("Changing address on textbox should update the state", () => {
+    const addressToType = "new add";
+    const action = {
+      type: ADDRESS_CHANGE,
+      data: { id: 0, text: addressToType }
+    };
+    const updatedState = reducer(state, action);
+
+    expect(updatedState[0].markers[0].address).toBe(addressToType);
   });
 });
